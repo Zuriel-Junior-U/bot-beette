7deefcc0832478a761f4ceb4dabc461e2b8b28e2
Zuriel Junior <zurielgames11@gmail.com>
2024-02-23 13:34:44 -0300

    feat: criando menu principal

--- bot.py
+++ bot.py
@@ -31,10 +31,17 @@ dp.include_router(form_router)
 @form_router.message(CommandStart())
 async def command_start(message: Message, state: FSMContext) -> None:
     builder = InlineKeyboardBuilder()
-    builder.button(text=f"Menu Principal", callback_data=f"menu")
+    builder.button(text=f"Menu Principal", callback_data=f"menu_principal")
     builder.button(text='Verificar ID', callback_data='verificar_id')
     builder.adjust(1, 1)
     await message.answer(text='Seja bem Vindo!', reply_markup=builder.as_markup())
+
+async def menu_principal(message: Message):
+    builder = InlineKeyboardBuilder()
+    builder.button(text='⚙️ Configurações', callback_data='data')
+    builder.button(text='🆘 Suporte', callback_data='data')
+    builder.adjust(2)
+    await message.answer(text='Menu Principal', reply_markup=builder.as_markup())
 
 @form_router.callback_query()
 async def my_call(call: types.CallbackQuery, state: FSMContext):
@@ -45,6 +52,8 @@ async def my_call(call: types.CallbackQuery, state: FSMContext):
     if call.data == 'verificar_id':
         await call.message.answer(f'ID: {meu_id}')
         await command_start(message, state)
+    if call.data == 'menu_principal':
+        await menu_principal(message)
 
 async def main() -> None:
     bot = Bot(TOKEN)
